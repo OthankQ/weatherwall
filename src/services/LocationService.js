e73446d8562639bf9acde61d868ec884c889995e
Ryan Kim <ryank9462@gmail.com>
2022-08-02 07:56:00 -0500

    create .env file and exclude api key from project

--- src/services/LocationService.js
+++ src/services/LocationService.js
@@ -5,7 +5,7 @@ const root = 'https://api.openweathermap.org/geo/1.0/direct';
 export async function getCoordByCityName(cityName) {
   try {
     const response = await axios.get(
-      `${root}?q=${cityName}&appid=30faf2f9812e649e9749c0fddc6f60a8`
+      `${root}?q=${cityName}&appid=${process.env.REACT_APP_API_KEY}`
     );
     return response.data;
   } catch (error) {
